34a3cab0d920dc98cdfc76ed56e47fb927c6e749
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2021-05-20 15:21:49 +0900

    wip

--- test/test.sql
+++ test/test.sql
@@ -1,21 +1,3 @@
 #standardSQL
-WITH
-  temp1 AS (
-    SELECT
-      ARRAY[3 * x, 3 * x + 1, 3 * x + 2] AS x
-    FROM
-      UNNEST(ARRAY[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]) AS x
-  ),
-  temp2 AS (
-    SELECT
-      2000 + y AS year
-    FROM
-      temp1
-      CROSS JOIN
-      UNNEST(temp1.x) AS y
-  )
 SELECT
-  year,
-  `myfunc.myfunc`(date(year, 3, 1))
-FROM
-  temp2;
+  `myfunc.myfunc`(date(2000, 3, 2));
