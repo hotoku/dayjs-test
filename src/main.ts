7d237ecca5186a23119dc2c962e89c8d28c8f013
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2021-05-23 10:25:29 +0900

    wip

--- src/main.ts
+++ src/main.ts
@@ -15,14 +15,23 @@ interface Ret {
   set0: string;
 }
 
+function adjust(date: Date, sign: number): void {
+  date.setTime(date.getTime() + sign * date.getTimezoneOffset() * 60 * 1000);
+}
+
 export function myfunc(date: Date): Ret {
+  adjust(date, 1);
   const offset = date.getTimezoneOffset();
   const orig = dayjs(date);
-  const set1 = orig.set("date", 1);
-  const set0 = orig.set("date", 0);
+  const orig2 = orig.toDate();
+  const set1 = orig.set("date", 1).toDate();
+  const set0 = orig.set("date", 0).toDate();
+  adjust(orig2, -1);
+  adjust(set1, -1);
+  adjust(set0, -1);
   return {
     offset: offset,
-    orig: orig.toISOString(),
+    orig: orig2.toISOString(),
     set1: set1.toISOString(),
     set0: set0.toISOString(),
   };
